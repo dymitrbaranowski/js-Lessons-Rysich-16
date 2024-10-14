f7cdd93550bf01ab668f4e8989a07004079a5484
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-14 17:50:09 +0300

    fox4

--- src/favorite.js
+++ src/favorite.js
@@ -2,6 +2,7 @@ import { common } from './common';
 import { createMarkup } from './helpers/createMarkup';
 
 const list = document.querySelector('.js-list');
+console.log(list);
 const close = document.querySelector('.js-close');
 console.log(close);
 
@@ -9,9 +10,9 @@ const favorite = JSON.parse(localStorage.getItem(common.KEY_FAVORITE)) ?? [];
 
 createMarkup(favorite, list);
 
-close.addEventListener('click', onClickClose);
+// close.addEventListener('click', onClickClose);
 
-function onClickClose(evt) {
-  console.log(evt);
-  localStorage.removeItem(common.KEY_FAVORITE);
-}
+// function onClickClose(evt) {
+//   console.log(evt);
+//   localStorage.removeItem(common.KEY_FAVORITE);
+// }
